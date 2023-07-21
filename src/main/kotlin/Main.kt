79ce39eda09857373638831291c78c53626f0029
AnaPaulinoo <ana.paulino@zup.com.br>
2023-07-21 19:01:32 -0300

    Subindo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,98 +1,33 @@
-import java.util.Scanner
+import java.util.concurrent.TimeUnit
+data class Item(val code: Int, val name: String, val price: Double)
+fun main() {
+    val lanches = listOf(
+        Item(1, "X-burger", 10.0),
+        Item(2, "X-salada", 12.0)
+    )
+    val bebidas = listOf(
+        Item(1, "Refrigerante", 8.0),
+        Item(2, "Suco", 6.0)
+    )
+    val carrinho = mutableListOf<Item>()
 
-fun main(args: Array<String>) {
+    val entrada = false
 
-    val scanner = Scanner(System.`in`)
-    menuInicial(scanner)
-}
-fun menuInicial (scanner:Scanner) {
-    println("Bem-vindo ao Fast Food Estrelas!")
-    println("1. Lanche")
-    println("2. Bebida")
-    var escolhaDoCliente: Int
+    while (!entrada) {
+        try {
+            println()
+            println("=== Bem-vindo ao FastFood Estrelas ===")
+            println("1. Lanche | 2. Bebida")
+            val opcao = readlnOrNull()?.toIntOrNull() ?: 0
 
-    try {
-        escolhaDoCliente = scanner.nextLine().trim().toInt()
-
-    } catch (e:Exception) {
-        throw IllegalArgumentException("Formato inválido, para escolher o item, você deve informar o número dele")
-
+            when (opcao) {
+                1 -> Registro().processarPedido(lanches, carrinho)
+                2 -> Registro().processarPedido(bebidas, carrinho)
+                3 -> Registro().finalizarPedido(carrinho)
+                else -> println("Opção inválida, tente novamente")
+            }
+        } catch (exception: NumberFormatException) {
+            println("Formato inválido, para escolher o item, você deve informar o número dele!")
+        }
     }
-    when (escolhaDoCliente) {
-       1 -> lanchesMenu(scanner)
-        2 -> bebidasMenu(scanner)
-        else -> tratarOpçaoInvalidaDoCliente(scanner)
-
-    }
-}
-
-fun bebidasMenu(scanner: Scanner) {
-
-}
-
-fun bebidasMenuMenu (scanner:Scanner) {
-    println("\nBEBIDAS DISPONÍVEIS:")
-    println("1. Refrigerantes - R$8,00")
-    println("2. Sucos - R$6,00")
-    var escolhaDoCliente: Int
-    try {
-        escolhaDoCliente = scanner.nextLine().trim().toInt()
-
-    } catch (e:Exception) {
-        throw IllegalArgumentException("Formato inválido, para escolher o item, você deve informar o número dele")
-
-    }
-
-    when (escolhaDoCliente) {
-        1 -> println("1. Refrigerantes - R$8,00")
-        2 -> println("2. 2. Sucos - R$6,00")
-        else ->  tratarOpçaoInvalidaDoLanche(scanner)
-
-    }
-}
-
-
-fun tratarOpçaoInvalidaDoCliente (scanner: Scanner) {
-    println("Opção invalida, por favor tente novamente")
-    menuInicial(scanner )
-}
-fun lanchesMenu (scanner:Scanner) {
-    println("\nLANCHES DISPONÍVEIS:")
-    println("1. X-burger - R$ 10,00")
-    println("2. X-salada - R$ 12,00")
-
-    val escolhaDoCliente: Int
-
-    try {
-        escolhaDoCliente = scanner.nextLine().trim().toInt()
-
-    } catch (e:Exception) {
-        throw IllegalArgumentException("Formato inválido, para escolher o item, você deve informar o número dele")
-
-    }
-
-     when (escolhaDoCliente) {
-        1 -> println("1. X-burger - R$ 10,00")
-        2 -> println("2. X-salada - R$ 12,00")
-        else ->  tratarOpçaoInvalidaDoLanche(scanner)
-
-    }
-}
-
-
-fun tratarCarrinho (scanner: Scanner) {
-    println("Quantas quantidades deseja?")
-
-    val quantidadeLanches = scanner.nextLine().trim().toInt()
-    val itenDoCarrinho= ItenDoCarrinho(codigo = (0..100).random(), quantidade= quantidadeLanches, nome = "X-burguer", preco = 10.00)
-    val carrinho = Carrinho(listaDeItens = mutableListOf<ItenDoCarrinho>())
-    var totalDoCarrinho= 0.0
-    for ( iten in carrinho.listaDeItens ) {
-        totalDoCarrinho= iten.preco * iten.quantidade + totalDoCarrinho
-    }
-    }
-
-fun tratarOpçaoInvalidaDoLanche (scanner: Scanner) {
-    println("Opção invalida, por favor tente novamente")
-    lanchesMenu(scanner )
 }
